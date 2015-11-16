674abdb6ec50063d42a9c9b25df5ff6b17d8d541
David Janes <davidjanes@davidjanes.com>
2015-11-16 15:08:58 -0500

    fixed JSON error

--- policies/AddPolicy.sh
+++ policies/AddPolicy.sh
@@ -12,6 +12,7 @@
 
 MY_AWS_ORG=${MY_AWS_ORG:=org}
 MY_AWS_GRP=${MY_AWS_GRP:=grp}
+MY_AWS_THING="thing-id"
 
 AWS_ID=$(aws iam get-user | python -c '
 import json, sys, re
@@ -43,12 +44,17 @@ while [ $# -gt 0 ] ; do
 			;;
 		--organization|--org)
 			shift
-            AWS_MY_ORG="$1"
+            MY_AWS_ORG="$1"
             shift
 			;;
 		--group|--grp)
 			shift
-            AWS_MY_GROUP="$1"
+            MY_AWS_GRP="$1"
+            shift
+			;;
+		--thing|--thing-id)
+			shift
+            MY_AWS_THING="$1"
             shift
 			;;
 		--help)
@@ -59,6 +65,7 @@ while [ $# -gt 0 ] ; do
             echo "--id <id>            change AWS User ID (is: $AWS_ID)"
             echo "--org <org_name>     change Organization used in topics (is: $MY_AWS_ORG)"
             echo "--group <group_name> change Group used in topics (is: $MY_AWS_GRP)"
+            echo "--thing <thing-id>   change Thing ID used in topics (is: $MY_AWS_THING)"
             echo
             echo "You can default 'org' and 'group' with the environment variables"
             echo "MY_AWS_ORG and MY_AWS_GRP"
@@ -80,6 +87,7 @@ do
     NAME="${BASENAME%.json}"
     NAME="${NAME/my_org/${MY_AWS_ORG}}"
     NAME="${NAME/my_grp/${MY_AWS_GRP}}"
+    NAME="${NAME/my_thing/${MY_AWS_THING}}"
 
     DOT_SRC=".${NAME}.json"
     URL="file://${DOT_SRC}"
@@ -89,6 +97,7 @@ do
         -e "1,$ s/123456789012/$AWS_ID/g" \
         -e "1,$ s/my_grp/$MY_AWS_GRP/g" \
         -e "1,$ s/my_org/$MY_AWS_ORG/g" \
+        -e "1,$ s/my_thing/$MY_AWS_THING/g" \
         < $SRC > $DOT_SRC
     cat $DOT_SRC
 
